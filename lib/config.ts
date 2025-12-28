cee19fd037db7f7c8e30afeeaff9330a91f8011c
samdev614 <creativeaglemedia@gmail.com>
2025-12-28 20:02:22 +0300

    Testing API calls for production

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://trizah-communications.onrender.com'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://tirzah-jan.onrender.com';
